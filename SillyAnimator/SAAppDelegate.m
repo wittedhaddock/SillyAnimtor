c3502f31937a6e4885c2e5fb6454fec6b57e8711
James William Graham <jameswilliamgraham@Jamess-MacBook-Pro.local>
2014-05-19 20:09:48 -0400

    images crop to the minimum

--- SillyAnimator/SAAppDelegate.m
+++ SillyAnimator/SAAppDelegate.m
@@ -10,6 +10,7 @@
 
 @interface SAAppDelegate()
 @property (strong, nonatomic) NSMutableArray *images;
+@property (strong, nonatomic) NSMutableArray *croppedImages;
 @property (weak) IBOutlet NSTextField *renameBaseTextField;
 @end
 @implementation SAAppDelegate
@@ -20,7 +21,12 @@
     }
     return _images;
 }
-
+- (NSMutableArray *)croppedImages{
+    if (!_croppedImages) {
+        _croppedImages = [[NSMutableArray alloc] init];
+    }
+    return _croppedImages;
+}
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
@@ -66,7 +72,13 @@
             
             NSURL *constructedURL = [NSURL URLWithString:renameBaseWithIncrementalInteger relativeToURL:[destinationDirectory directoryURL]];
             NSLog(@"%@", constructedURL);
-            [self saveImageAsPNG:self.images[i] withName:nil atURL:constructedURL];
+            if ([self.croppedImages count] > 0) {
+                [self saveImageAsPNG:self.croppedImages[i] withName:nil atURL:constructedURL];
+            }
+            else {
+                [self saveImageAsPNG:self.images[i] withName:nil atURL:constructedURL];
+
+            }
         }
     }
     
@@ -79,6 +91,107 @@
     imageData = [imageRep representationUsingType:NSPNGFileType properties:imageProperties];
     [imageData writeToURL:URL atomically:YES];
     
+}
+- (CGContextRef)createARGBBitmapContextFromImage:(CGImageRef)inImage {
+    
+    CGContextRef context = NULL;
+    CGColorSpaceRef colorSpace;
+    void *bitmapData;
+    int bitmapByteCount;
+    int bitmapBytesPerRow;
+    
+    // Get image width, height. We'll use the entire image.
+    size_t width = CGImageGetWidth(inImage);
+    size_t height = CGImageGetHeight(inImage);
+    
+    // Declare the number of bytes per row. Each pixel in the bitmap in this
+    // example is represented by 4 bytes; 8 bits each of red, green, blue, and
+    // alpha.
+    bitmapBytesPerRow = (width * 4);
+    bitmapByteCount = (bitmapBytesPerRow * height);
+    
+    // Use the generic RGB color space.
+    colorSpace = CGColorSpaceCreateDeviceRGB();
+    if (colorSpace == NULL) return NULL;
+    
+    // Allocate memory for image data. This is the destination in memory
+    // where any drawing to the bitmap context will be rendered.
+    bitmapData = malloc( bitmapByteCount );
+    if (bitmapData == NULL)
+    {
+        CGColorSpaceRelease(colorSpace);
+        return NULL;
+    }
+    
+    // Create the bitmap context. We want pre-multiplied ARGB, 8-bits
+    // per component. Regardless of what the source image format is
+    // (CMYK, Grayscale, and so on) it will be converted over to the format
+    // specified here by CGBitmapContextCreate.
+    context = CGBitmapContextCreate (bitmapData,
+                                     width,
+                                     height,
+                                     8,      // bits per component
+                                     bitmapBytesPerRow,
+                                     colorSpace,
+                                     kCGImageAlphaPremultipliedFirst);
+    if (context == NULL) free (bitmapData);
+    
+    // Make sure and release colorspace before returning
+    CGColorSpaceRelease(colorSpace);
+    
+    return context;
+}
+- (CGImageRef)imageRefForNSImage:(NSImage *)image{
+    return [image CGImageForProposedRect:NULL context:[NSGraphicsContext currentContext] hints:nil];
+}
+- (CGRect)cropRectForImage:(NSImage *)image {
+    
+    CGImageRef cgImage = [self imageRefForNSImage:image];
+    CGContextRef context = [self createARGBBitmapContextFromImage:cgImage];
+    if (context == NULL) return CGRectZero;
+    
+    size_t width = CGImageGetWidth(cgImage);
+    size_t height = CGImageGetHeight(cgImage);
+    CGRect rect = CGRectMake(0, 0, width, height);
+    
+    CGContextDrawImage(context, rect, cgImage);
+    
+    unsigned char *data = CGBitmapContextGetData(context);
+    CGContextRelease(context);
+    
+    //Filter through data and look for non-transparent pixels.
+    int lowX = width;
+    int lowY = height;
+    int highX = 0;
+    int highY = 0;
+    if (data != NULL) {
+        for (int y=0; y<height; y++) {
+            for (int x=0; x<width; x++) {
+                int pixelIndex = (width * y + x) * 4 /* 4 for A, R, G, B */;
+                if (data[pixelIndex] != 0) { //Alpha value is not zero; pixel is not transparent.
+                    if (x < lowX) lowX = x;
+                    if (x > highX) highX = x;
+                    if (y < lowY) lowY = y;
+                    if (y > highY) highY = y;
+                }
+            }
+        }
+        free(data);
+    } else {
+        return CGRectZero;
+    }
+    
+    return CGRectMake(lowX, lowY, highX-lowX, highY-lowY);
+}
+- (IBAction)crop:(id)sender {
+        for (int i = 0; i < [self.images count]; i++) {
+            CGRect minimumImageRect = [self cropRectForImage:self.images[i]];
+            CGImageRef croppedImageRef = CGImageCreateWithImageInRect([self imageRefForNSImage:self.images[i]], minimumImageRect);
+            NSImage *croppedImage = [[NSImage alloc] initWithCGImage:croppedImageRef size:NSZeroSize];
+            [self.croppedImages addObject:croppedImage];
+            
+    
+    }
 }
 
 @end
